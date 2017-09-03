14f7577cd195e22e8df701a572320f14fc34d797
Daniel Stahl <danstahl1138@gmail.com>
2017-09-03 13:06:29 -0500

    functioning

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
-| [Linux][lin-link] | [Windows][win-link] | [Codecov][cov-link] |
-| :---------------: | :-----------------: | :-------------------: |
-| ![lin-badge]      | ![win-badge]        | ![cov-badge]          |
+## Inverse tau
 
-[lin-badge]: https://travis-ci.org/phillyfan1138/cfdistutilities.svg?branch=master "Travis build status"
-[lin-link]:  https://travis-ci.org/phillyfan1138/cfdistutilities "Travis build status"
-[win-badge]: https://ci.appveyor.com/api/projects/status/y36u1hdyxjj9r2a0?svg=true "AppVeyor build status"
-[win-link]:  https://ci.appveyor.com/project/phillyfan1138/cfdistutilities "AppVeyor build status"
-[cov-badge]: https://codecov.io/gh/phillyfan1138/cfdistutilities/branch/master/graph/badge.svg
-[cov-link]:  https://codecov.io/gh/phillyfan1138/cfdistutilities
+This is the c++ implementation of the paper on generating distributions for first hitting times from GBMs.  See [Tau Distribution](https://github.com/phillyfan1138/TauDistribution).
 
-# CFDistUtilities
-This repository holds utilities for computing risk metrics like VaR and expected shortfall for any distribution with an analytic Characteristic Function.  See the [tests](./test.cpp) for examples.
-
-In numerical tests it is extremely quick: for a portfolio of 1,000,000 loans the computation of VaR was faster than the computation of the graph of the density.  See my [FaaSDemo](https://github.com/phillyfan1138/ModelFaaSDemo).
-
-## Potential limitations
-
-* For densities without derivatives of all orders, the convergence may be slow.  For example, Beta distributions may not converge at all when the mode of the distribution is near zero or one.  
-* The VaR technique works when the cumulative density is monotonic.  While this is manifestly the case for any density, it is not the case that the cosine approximation is monotonic.  Hence the VaR may not converge to the actual VaR.  However, in tests it appears that it does; at least for "nice" distributions.  
-* The Newton VaR technique is faster than the bisection technique; however for large domains the derivative is tiny and Newton's algorithm diverges.  The bisect method is safer.
+However, it doesn't work yet.  Something is wrong when it inverts the matrix using Thomas' algorithm (see ODESolver).  I've checked that the R code and C++ match perfectly going into an inversion algorithm, but not coming out.
--- main.cpp
+++ main.cpp
@@ -3,38 +3,58 @@
 #include "FangOost.h"
 #include "ODESolver.h"
 #include <iostream>
+
+std::vector<std::vector<double> > transpose(const std::vector<std::vector<std::complex<double> > >& data, double cp) {
+    std::vector<std::vector<double> > result(data[0].size(),
+                                          std::vector<double>(data.size()));
+    for (std::vector<double>::size_type i = 0; i < data[0].size(); i++) 
+        for (std::vector<double>::size_type j = 0; j < data.size(); j++) {
+            result[i][j] = data[j][i].real()*cp;
+        }
+    return result;
+}
 int main(){
-    constexpr int discreteX=100; //discrete X
-    constexpr int discreteU=64; 
+    constexpr int discreteX=298; //discrete X
+    constexpr int discreteTau=1024; //discrete t
+    constexpr int discreteU=512; 
     auto delta=.8;
     auto alpha=.1;
     auto sigma=.3;
     //auto S0=50.0;
 
-    auto M=100.0;
+    auto M=5.0; //hitting boundary
     auto xMin=0.0;
     auto initLower=0.0;//0 probability at 0
     auto initHigher=1.0; //probabilty of 1 at M
     auto xMax=M;
+    auto tMin=0.0;
+    auto tMax=176.7939;
+
     auto alphaFn=[&](const auto& x){
-        return std::complex<double>(x*alpha, 0.0);
+        return alpha*x;
     };
     auto sigmaFn=[&](const auto& x){
-        return std::complex<double>(x*sigma*sigma*x*.5, 0.0);
+        return pow(x, 2.0*delta)*sigma*sigma*.5;
     };
     auto uFn=[&](const auto& u){
         return std::complex<double>(0.0, -u);
     };
+    auto du=fangoost::computeDU(tMin, tMax);
+    auto cp=fangoost::computeCP(du);
     auto matrixOfXByU=futilities::for_each_parallel(0, discreteU, [&](const auto& index){
-        return odesolver::solveODE<discreteX>(alphaFn, sigmaFn, [&](const auto& x){
-            return uFn(fangoost::getU(fangoost::computeDU(xMin, xMax), index));
-        }, xMin, xMax, initLower, initHigher);
+        auto fnConstant=uFn(fangoost::getU(du, index));
+        return odesolver::solveODE_diff(sigmaFn, alphaFn, [&](const auto& x){
+            return fnConstant;
+        }, initLower, initHigher, xMin, xMax, discreteX);
     });
-
-    auto vectorOfDistributions=futilities::for_each_parallel(0, discreteU, [&](const auto& index){
-        return fangoost::computeInvDiscrete(discreteX, 0.0, 10.0, matrixOfXByU[index]);
+    auto dT=fangoost::computeDX(discreteTau, tMin, tMax);
+    auto transposeVector=transpose(matrixOfXByU, cp);
+    auto vectorOfDistributions=futilities::for_each_parallel(0, discreteX, [&](const auto& index){
+        return fangoost::computeInvDiscrete(discreteTau, tMin, tMax, transposeVector[index]);
     });
-    for(auto& val: vectorOfDistributions[50]){
-        std::cout<<val<<std::endl;
+    
+    std::cout<<"x, density"<<std::endl;
+    for(int i=0;i<discreteTau;++i){
+        std::cout<<fangoost::getX(tMin, dT, i)<<", "<<vectorOfDistributions[60][i]<<std::endl;
     }
 }
